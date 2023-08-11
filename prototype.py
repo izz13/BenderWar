b463594f46ad8c3c0688db19e9bc32b75d61557c
jaydenchoi <jaydenchoi2013@gmail.com>
2023-08-11 11:59:17 -0700

    added green

--- prototype.py
+++ prototype.py
@@ -6,12 +6,15 @@ screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 fps = 60
 
+green = (0, 255, 0)
+
 isRunning = True
 while isRunning==True:
     events = pygame.event.get()
     for event in events:
         if event.type==pygame.QUIT:
             isRunning = False
+    screen.fill(green)
     pygame.display.flip()
     clock.tick(fps)
 
