0c06b4b3d7af4c78aded19668bcd17345691f653
bop12345678 <76543210eric@gmail.com>
2023-08-11 12:31:12 -0700

    render and update plus hitbox

--- player.py
+++ player.py
@@ -6,3 +6,13 @@ class Player:
         self.dmg = dmg
         self.elmt = elmt
         self.image = pygame.image.load(image)
+        self.x = 0
+        self.y = 0
+        self.rect = self.image.get_bounding_rect()
+    def render(self, screen):
+        self.rect.center.x = self.x
+        self.rect.center.y = self.y
+        screen.blit(self.image, self.rect)
+
+    def update(self, screen):
+        self.render(screen)
