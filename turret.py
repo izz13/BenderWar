e07bf7265bd748d915076e239c4b07b6f21e8145
jaydenchoi <jaydenchoi2013@gmail.com>
2023-09-05 21:04:32 -0700

    added projectile

--- turret.py
+++ turret.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+from pygame.math import Vector2
 
 class Turret:
     def __init__(self, x, y, hp, image, elmt, attackRange=150):
@@ -10,20 +11,44 @@ class Turret:
         self.elmt = elmt
         self.attackRange = attackRange
         self.rect = self.image.get_bounding_rect()
+        self.projectiles = []
     def render(self, screen):
         self.rect.centerx = self.x
         self.rect.centery = self.y
-        screen.blit(self.image,self.rect)
+        screen.blit(self.image, self.rect)
 
     def distance(self, Px, Py):
         dx = Px-self.x
         dy = Py-self.y
         distance = math.sqrt(dx*dx+dy*dy)
-        return distance
+        return distance, dx, dy
 
     def attack(self, px, py):
-        if self.distance(px, py)<=self.attackRange:
-            print("attacking")
+        d, dx, dy = self.distance(px, py)
+        if d <= self.attackRange:
+            pdirection = Vector2(dx, dy)
+            self.projectiles.append(Projectile(self.x, self.y, 3, pdirection))
     def update(self, screen, px, py):
         self.render(screen)
         self.attack(px, py)
+        if len(self.projectiles) > 0:
+            for p in self.projectiles:
+                p.update(screen)
+class Projectile:
+    def __init__(self, x, y, speed, direction):
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.direction = direction
+        self.rect = pygame.Rect(self.x, self.y, 32, 32)
+    def render(self, screen):
+        self.rect.center = [self.x, self.y]
+        pygame.draw.rect(screen, [99, 66, 00], self.rect)
+    def move(self):
+        vel = self.direction.scale_to_length(self.speed)
+        print(vel)
+        self.x += vel[0]
+        self.y += vel[1]
+    def update(self, screen):
+        self.move()
+        self.render(screen)
