86c2e65d0f2e7038ec16fcfcb0890633aaaa79ef
bop12345678 <76543210eric@gmail.com>
2023-10-03 19:07:05 -0700

    import

---
+++ WaterWorld.py
@@ -0,0 +1,49 @@
+import pygame
+
+
+pygame.init()
+size = [800, 640]
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+fps = 60
+
+
+isRunning = True
+while isRunning==True:
+    tick=clock.get_time()
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            isRunning = False
+
+    pygame.display.flip()
+    clock.tick(fps)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pygame.quit()
